f8e5ef593e757845d01d92ec569da9bc7cfd6c33
bautielipinali <elipinalibauti1@gmail.com>
2023-09-09 18:58:30 -0300

    Maquetacion Login - Finalizada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<Home/>}/>
-        {/* <Route path='/login'/>
-        <Route path='/detail:id'/> */}
+        <Route path='/login' element={<Login/>}/>
+        {/*<Route path='/detail:id'/> */}
       </Routes>
     </>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { Grid } from '@mui/material'
-import {makeStyles} from '@mui/material/styles'
-import fondospm from '../../images/fondospm.png'
-
-const UseStyles = makeStyles(theme=>({
-    root:{
-      backgroundImage:`url(${fondospm})`
-        
-    }
-}))
+import React from 'react';
+import './Login.css'; 
+import fondospm from '../Login/fondospm.png';
+import { Grid, Paper, TextField, Button} from '@mui/material';
 
 
 function Login () {
-  const classes = UseStyles()
-  
-  return (
-   <Grid container component='main' className={classes.root}> 
 
+ /*  <img className='fondo-spiderman' src={fondospm} alt="Spiderman Background" /> */
+
+
+  const paperStyle={padding: 20, height: '45vh', width:280, margin: "20px auto"}
+   return (
+   <Grid sx={{marginTop: '120px'}}>
+    <Paper elevation={10} style={paperStyle}>
+      <h2>Sing in</h2>
+        <form action="">  
+          <TextField sx={{margin: '10px auto'}} label='Username' placeholder='Enter username' type='text' fullWidth required/>
+          <TextField sx={{margin: '10px auto'}} label='Password' placeholder='Enter password' type='password' fullWidth required/>
+          <Button sx={{marginTop: '20px'}} type='submit' color='primary' variant='contained' fullWidth> Sign In</Button>
+
+       </form>
+    </Paper>
    </Grid>
-  )
+    );
 }
 
 export default Login
